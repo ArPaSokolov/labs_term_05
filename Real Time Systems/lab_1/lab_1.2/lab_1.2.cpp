a4821e6e54944958807cac175e99a5c9ffcbe64e
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-09-17 11:12:25 +0200

    Passed

---
+++ Real Time Systems/lab_1/lab_1.2/lab_1.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <time.h>
+
+void Fact(int N) { // вычисление N!
+
+	int f = 1;
+	for (int i = 1; i <= N; i++) {
+		f *= i;
+	}
+}
+
+
+int main() {
+	clock_t start = clock(); // время до выполнения кода
+
+	int N = 10;
+	int i = 1;
+	
+	while (i < 10000000) { // 10 млн. раз вычисляем 10! 
+		Fact(N);
+		i++;
+	}
+
+	clock_t end = clock(); // время после выполнения кода
+	double seconds = (double)(end - start) / CLOCKS_PER_SEC; // время выполнения кода в секундах
+
+	std::cout << seconds << std::endl;
+}
+
