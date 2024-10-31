595982870e5c6c487e9eaff92984ef6e197d1bf4
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-10-31 14:23:37 +0200

    Add lab_2

--- Real Time Systems/lab_2/lab_2.cpp
+++ Real Time Systems/lab_2/lab_2.cpp
@@ -1,23 +1,27 @@
-#include <thread>
+﻿#include <thread>
 #include <mutex>
 #include <string>
 #include <iostream>
+#include <cmath>
+#include <chrono>
 
-
-std::mutex m; 
 
 void Func(std::string name)
 {
     long double i = 0;
+    auto start = std::chrono::high_resolution_clock::now();
 
-    m.lock();
+    while (std::chrono::high_resolution_clock::now() - start < std::chrono::seconds(1)) {
+        i += pow(10, -9);
+    }
+
     std::cout << name << ": " << i << std::endl;
-    m.unlock();
 
 }
 
 int main()
 {
+    std::cout << std::endl;
     std::thread thread1(Func, "t1");
     std::thread thread2(Func, "t2");
     std::thread thread3(Func, "t3");
@@ -26,4 +30,6 @@ int main()
     thread3.join();
 
     system("pause");
+
+    return 0;
 }
