f0d75d66181b96443b9212511cea43e0781a4886
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-11-24 20:27:35 +0200

    Add lab_7

---
+++ Real Time Systems/lab_7/custom_mutex.cpp
@@ -0,0 +1,19 @@
+﻿#include "custom_mutex.h"
+
+CustomMutex::CustomMutex() {
+    semaphore = CreateSemaphore(NULL, 1, 1, NULL); // создание семафора
+}
+
+void CustomMutex::lock() {
+    WaitForSingleObject(semaphore, INFINITE); // ожидание семафора
+}
+
+void CustomMutex::unlock() {
+    ReleaseSemaphore(semaphore, 1, NULL); // освобождение семафора
+}
+
+CustomMutex::~CustomMutex() {
+    CloseHandle(semaphore); // закрытие семафора
+}
+
+// о семафоре на событиях https://habr.com/ru/articles/476940/
